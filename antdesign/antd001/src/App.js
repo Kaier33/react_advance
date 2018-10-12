27de2f3fa0a4a7069f6c326cf07f8f16ddc83db1
Kaier_Chou <469439801@qq.com>
2018-10-13 01:02:18 +0800

    antdesin 启动~

---
+++ antdesign/antd001/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Button from 'antd/lib/button';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+
+          <Button type="primary">Button</Button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ antdesign/antdva/src/routes/IndexPage.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { connect } from 'dva';
+import { Link } from 'dva/router'
+import styles from './IndexPage.less';
+import logo from '../assets/logo.svg';
+
+import { Layout, Menu, Icon } from 'antd';
+const { Header, Sider, Content } = Layout;
+const SubMenu = Menu.SubMenu;
+
+
+class App extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+          breakpoint="lg"
+          width={256}
+          className={styles.sider}
+        >
+          <div className={styles.logo} key="logo">
+            <Link to="/">
+              <img src={logo} alt="logo" />
+              <h1>Ant Design Demo</h1>
+            </Link>
+          </div>
+
+          <Menu
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            mode="inline"
+            theme="dark"
+            inlineCollapsed={this.state.collapsed}
+            style={{ padding: '16px 0', width: '100%' }}
+          >
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Option 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="inbox" />
+              <span>Option 3</span>
+            </Menu.Item>
+            <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+              <Menu.Item key="5">Option 5</Menu.Item>
+              <Menu.Item key="6">Option 6</Menu.Item>
+              <Menu.Item key="7">Option 7</Menu.Item>
+              <Menu.Item key="8">Option 8</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
+              <Menu.Item key="9">Option 9</Menu.Item>
+              <Menu.Item key="10">Option 10</Menu.Item>
+              <SubMenu key="sub3" title="Submenu">
+                <Menu.Item key="11">Option 11</Menu.Item>
+                <Menu.Item key="12">Option 12</Menu.Item>
+              </SubMenu>
+            </SubMenu>
+          </Menu>
+        </Sider>
+
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className={styles.trigger}
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  }
+
+}
+
+
+export default connect()(App);
